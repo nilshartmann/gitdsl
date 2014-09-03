14bddd67802c3a2611355f1b7e208c06db670ffa
Nils Hartmann <nils@nilshartmann.net>
2014-09-03 09:29:34 +0200

    initial import

---
+++ gitdsl/src/main/groovy/moin/hello.groovy
@@ -0,0 +1,13 @@
+import java.util.LinkedList;
+import java.util.List;
+
+import org.eclipse.jgit.api.*;
+
+println "hello"
+
+
+List<String> strings = new LinkedList<>()
+
+strings.add("hallo");
+println strings;
+def git = new Git();
---
+++ gitdsl/src/main/java/moin/JavaTest.java
@@ -0,0 +1,10 @@
+package moin;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class JavaTest {
+
+	private final List<String> strings = new LinkedList<>();
+	
+}
