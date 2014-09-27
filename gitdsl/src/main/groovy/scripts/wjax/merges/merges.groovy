7906c06b5b0617d27615dd71990474e3204b271f
Nils Hartmann <nils@nilshartmann.net>
2014-09-28 01:14:17 +0200

    Weitere Beispiele

--- gitdsl/src/main/groovy/scripts/wjax/merges/merges.groovy
+++ gitdsl/src/main/groovy/scripts/wjax/merges/merges.groovy
@@ -11,7 +11,7 @@ final def BASE_DIR='/Users/nils/develop/wjax2014_git_workshop/uebungen/merges';
 
 final def LOKALES_REPO="${BASE_DIR}/01_ffRepo"
 
-Utils.recreateFile("$BASE_DIR/beispiel1_fastforward.txt") << """
+Utils.recreateFile("$BASE_DIR/beispiel_01_fastforward.txt") << """
 SZENARIO 1: FF-MERGE
 
 	cd ${LOKALES_REPO}
@@ -52,8 +52,10 @@ SZENARIO 1: FF-MERGE
 def commits(Map args = new Hashtable(), gitdsl.RepositoryScript gs, String branch) {
 	gs.checkout branch, startPoint: 'master';
 
-	for (int i=1;i<4;i++) {
-		gs.modifyFile 'f1'; gs.commit "$branch: $i. Commit";
+	final int commits = args.get('commits', 3);
+
+	for (int i=1;i<(commits+1);i++) {
+		gs.modifyFile 'f1', content: args.content; gs.commit "$branch: $i. Commit";
 	}
 
 
@@ -76,7 +78,7 @@ GitRepository.recreateAt("${LOKALES_REPO}").setup {
 
 // ===== BEISPIEL 2 ==================================================================
 final def FIRST_PARENT_REPO="${BASE_DIR}/02_firstParentRepo"
-Utils.recreateFile("$BASE_DIR/beispiel_firstparent.txt") << """
+Utils.recreateFile("$BASE_DIR/beispiel_02_firstparent.txt") << """
 
 # Beispiel: First Parent History
 
@@ -105,5 +107,98 @@ GitRepository.recreateAt("${FIRST_PARENT_REPO}").setup {
 
 }
 
+// ===== BEISPIEL 2 ==================================================================
+final def CONFLICT_REPO="${BASE_DIR}/03_mergeConflictRepo"
+Utils.recreateFile("$BASE_DIR/beispiel_03_mergeConflict.txt") << """
 
+	# Ausgangssituation mit gitk zeigen: Zwei Branches, beide vom Master, beide README veraendert
+	
+	# Merge FEATURE-1
+	git merge --no-ff FEATURE-1
+	git branch -d FEATURE-1	
+
+	# ggf. Historie zeigen
+	git merge --no-ff FEATURE-2
+
+	# MERGE KONFLIKT!!!
+
+	# git status => wie erwartet
+	git status
+
+	# Lösung 1: Reset und 'ours' nehmen
+	git reset --hard
+	# oder: git merge --abort
+
+	git merge -s ours FEATURE-2
+
+	# readme zeigen: enthaelt nur Aenderungen von FEATURE-1
+	cat readme.txt
+
+	# Doof: install.txt ueberschrieben
+	cat install.txt
+	git diff FEATURE-2 -- install.txt
+
+	# Lösung: Nur Konflikt-behaftete Dateien von "uns" übernehmen
+	git merge -X ours FEATURE-2
+
+
+	# Lösung 2: Nur readme.txt ignorieren
+	git reset --hard HEAD^
+
+	git merge FEATURE-2
+	git checkout --ours -- readme.txt
+	
+	# Status zeigen (readme.txt nicht gestaged), aber conflikt marker sind weg
+	cat readme.txt
+
+	# mit checkout HEAD wuerde auch gleich gestaged
+	# git checkout HEAD -- readme.txt
+	
+	# oder git checkout --ours -- readme.txt
+	git status
+	# readme.txt ist weg...
+
+	# Lösung 3: readme.txt überschreiben
+	git checkout -m -- readme.txt
+	
+	# Konfliktmarker wieder da!
+	cat readme.txt
+
+	# 'andere' Version nehmen
+	git checkout --theirs -- readme.txt
+
+	# git mergetool
+	git checkout -m -- readme.txt
+
+	git mergetool
+
+	# Gestaged
+	git status
+	
+	
+
+
+	# Files anzeigen: 3 Stueck ???
+	git ls-files 
+	git ls-files --stage
+
+
+
+	
+
+
+"""
+GitRepository.recreateAt("${CONFLICT_REPO}").setup {
+	// Initialer Commit
+	addFile 'f1', path: 'readme.txt'
+	addFile 'f2', path: 'install.txt', content: 'Dieses wird die Installationsanleitung'
+	commit 'Initial Import'
+
+	commits delegate, 'FEATURE-1', commits: 1, content: 'Aenderung auf FEATURE-1'
+	commits delegate, 'FEATURE-2', commits: 1, content: 'Aenderung auf FEATURE-2'
+	modifyFile 'f2', add: 'Zur Installation gehen Sie wie folgt vor:\n\n 1. Lorem\n 2. Ipsum\n 3.Dolor sit amend'; commit "FEATURE-2: install.txt erweitert"
+
+	checkout 'master', create: false
+
+}
 
