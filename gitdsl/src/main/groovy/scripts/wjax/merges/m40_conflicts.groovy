3f3607b90960d310ad0ae06787c593645d2ce54f
Nils Hartmann <nils@nilshartmann.net>
2014-10-07 14:47:36 +0200

    conflicts beispiel

--- gitdsl/src/main/groovy/scripts/wjax/merges/m40_conflicts.groovy
+++ gitdsl/src/main/groovy/scripts/wjax/merges/m40_conflicts.groovy
@@ -13,12 +13,12 @@ Utils.recreateFile("$BASE_DIR/beispiel_40_conflicts.txt") << """
 
 	# Ausgangssituation mit gitk zeigen: Zwei Branches, beide vom Master, beide README veraendert
 	
-	# Merge FEATURE-1
-	git merge --no-ff FEATURE-1
-	git branch -d FEATURE-1	
+	# Merge feature-1
+	git merge --no-ff feature-1
+	git branch -d feature-1	
 
 	# ggf. Historie zeigen
-	git merge --no-ff FEATURE-2
+	git merge --no-ff feature-2
 
 	# MERGE KONFLIKT!!!
 
@@ -29,23 +29,23 @@ Utils.recreateFile("$BASE_DIR/beispiel_40_conflicts.txt") << """
 	git reset --hard
 	# oder: git merge --abort
 
-	git merge -s ours FEATURE-2
+	git merge -s ours feature-2
 
-	# readme zeigen: enthaelt nur Aenderungen von FEATURE-1
+	# readme zeigen: enthaelt nur Aenderungen von feature-1
 	cat readme.txt
 
 	# Doof: install.txt ueberschrieben
 	cat install.txt
-	git diff FEATURE-2 -- install.txt
+	git diff feature-2 -- install.txt
 
 	# Lösung: Nur Konflikt-behaftete Dateien von "uns" übernehmen
-	git merge -X ours FEATURE-2
+	git merge -X ours feature-2
 
 
 	# Lösung 2: Nur readme.txt ignorieren
 	git reset --hard HEAD^
 
-	git merge FEATURE-2
+	git merge feature-2
 	git checkout --ours -- readme.txt
 	
 	# Status zeigen (readme.txt nicht gestaged), aber conflikt marker sind weg
@@ -101,10 +101,10 @@ GitRepository.recreateAt("${CONFLICT_REPO}").setup {
 	addFile 'f2', path: 'install.txt', content: 'Dieses wird die Installationsanleitung'
 	commit 'Initial Import'
 
-	commits delegate, 'FEATURE-1', commits: 1, content: "Dieses ist die erstmals angepasste Readme-Datei\nBeschreibung von FEATURE-1:\nDieses brandneue Feature erlaubt es Ihnen, noch besser mit unserer Software zu arbeiten."
-	commits delegate, 'FEATURE-2', commits: 1, content: "Dieses ist die Readme-Datei\nSie beschreibt alle Features Ihres gekauften Produktes\n\nNeu! FEATURE-2:\nJetzt ist unser Tool noch besser.\nSie sollten es unbedingt sofort installieren und testen."
-	modifyFile 'f2', add: 'Zur Installation gehen Sie wie folgt vor:\n\n 1. Lorem\n 2. Ipsum\n 3.Dolor sit amend'; commit "FEATURE-2: install.txt erweitert"
-	addFile 'f3', path: 'version.txt', content: 'Version 2'; commit "FEATURE-2: version.txt angelegt"
+	commits delegate, 'feature-1', commits: 1, content: "Dieses ist die erstmals angepasste Readme-Datei\nBeschreibung von feature-1:\nDieses brandneue Feature erlaubt es Ihnen, noch besser mit unserer Software zu arbeiten."
+	commits delegate, 'feature-2', commits: 1, content: "Dieses ist die Readme-Datei\nSie beschreibt alle Features Ihres gekauften Produktes\n\nNeu! feature-2:\nJetzt ist unser Tool noch besser.\nSie sollten es unbedingt sofort installieren und testen."
+	modifyFile 'f2', add: 'Zur Installation gehen Sie wie folgt vor:\n\n 1. Lorem\n 2. Ipsum\n 3.Dolor sit amend'; commit "feature-2: install.txt erweitert"
+	addFile 'f3', path: 'version.txt', content: 'Version 2'; commit "feature-2: version.txt angelegt"
 
 	checkout 'master', create: false
 
